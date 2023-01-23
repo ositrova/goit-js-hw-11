c30fc58514dbd0c1a22793532d59353f1b15e1ea
ositrova <110326755+ositrova@users.noreply.github.com>
2023-01-23 08:58:31 +0200

    taskCountry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,87 @@
 import './css/styles.css';
 
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries.js';
+
 const DEBOUNCE_DELAY = 300;
 
+const boxSearch = document.querySelector('#search-box');
+const listCountry = document.querySelector('.country-list');
+const infoCountry = document.querySelector('.country-info');
 
+const onCountryFormInput = event => {
+    const searchedQuery = boxSearch.value.trim();
+
+    if (!searchedQuery) {
+listCountry.innerHTML = '';
+infoCountry.innerHTML = '';
+    return
+    };
+
+    fetchCountries(searchedQuery)
+    .then(data => {
+      if (data.length > 10) {
+        infoCountry.innerHTML = '';
+        listCountry.innerHTML = '';
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length >= 2 && data.length <= 10) {
+        createCountryList(data);
+        infoCountry.innerHTML = '';
+      } else if (data.length === 1) {
+        createCountryCard(data[0]);
+        listCountry.innerHTML = '';
+      }
+    }).catch(err => {
+        switch (err.message) {
+          case '404': {
+            Notify.failure('Oops, there is no country with that name');
+            infoCountry.innerHTML = '';
+            listCountry.innerHTML = '';
+            break;
+          }
+        }
+      });
+  };
+  
+  function createCountryList(data) {
+    const markup = data
+      .map(({ name, flags }) => {
+        return `<li class="country-list-item">
+        <img src="${flags.svg}" alt="flag" height="15px">
+        <p>${name}</p>
+      </li>`;
+      })
+      .join('');
+      listCountry.innerHTML = markup;
+  }
+  
+  function createCountryCard(data) {
+    const markup = `
+        <div class="country-list-item">
+        <img src="${data.flags.svg}" alt="flag" height="15px">
+        <h1>${data.name}</h1>
+        </div>
+        <ul class="country-list">
+        <li><p class="country-list-text"><b>Capital</b>: ${data.capital}</p></li>
+        <li><p class="country-list-text"><b>Population</b>: ${
+          data.population
+        }</p></li>
+        <li><p class="country-list-text"><b>Languages</b>: ${Object.values(
+          data.languages
+        )
+          .map(el => el.name)
+          .join(', ')}</p></li> 
+      </ul>`;
+      infoCountry.innerHTML = markup;
+  }
+  
+  boxSearch.addEventListener(
+    'input',
+    debounce(onCountryFormInput, DEBOUNCE_DELAY)
+  );
 
 
 // HTTP-запит​
